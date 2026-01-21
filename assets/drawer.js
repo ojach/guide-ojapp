c12cc2178cc3ba05b0c63b9e5f5e74606bc81849
ojach <trc.wasps@gmail.com>
2026-01-21 19:07:00 +0900

    Update drawer.js

--- assets/drawer.js
+++ assets/drawer.js
@@ -1,1 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const drawer = document.getElementById("drawer");
+  const btn = document.getElementById("drawerBtn");
+
+  btn.addEventListener("click", () => {
+    drawer.classList.toggle("open");
+  });
+});
 

1b5c3c0e21c95f88f8dcf408d6fdf64351821bda
ojach <trc.wasps@gmail.com>
2026-01-23 23:42:04 +0900

    Update drawer.js

--- assets/drawer.js
+++ assets/drawer.js
@@ -1,10 +1,21 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const btn = document.getElementById("menuToggle");
-  const menu = document.getElementById("menu");
+// ヘッダー読込が終わったら動くようにする
+window.addEventListener("DOMContentLoaded", () => {
+  // fetch の完了を待つ
+  const headerObserver = new MutationObserver(() => {
+    const btn = document.getElementById("menuToggle");
+    const menu = document.getElementById("menu");
 
-  if (!btn || !menu) return; // 読み込み保険
+    if (!btn || !menu) return;
 
-  btn.addEventListener("click", () => {
-    menu.classList.toggle("hidden");
+    btn.addEventListener("click", () => {
+      menu.classList.toggle("hidden");
+    });
+
+    // 1回動いたら監視を止める
+    headerObserver.disconnect();
+  });
+
+  headerObserver.observe(document.getElementById("header"), {
+    childList: true,
   });
 });
